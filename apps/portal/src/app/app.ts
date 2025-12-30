f7460eb92de3547d53d5ff878bfc437643a472d4
ryandremza <ryan.dremza@gmail.com>
2025-12-30 09:29:11 +0000

    portal - update landing page

--- apps/portal/src/app/app.ts
+++ apps/portal/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [CommonModule, RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected title = 'portal';
-}
+export class App {}
